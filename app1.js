de7dd7371044da0b1c4e75915e3566735fd7509e
scnetsec <santacruzws@gmail.com>
2017-04-18 00:10:43 -0700

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,34 @@
+var main = function () {
+    var eNote = document.getElementById('eAudio');
+    var aNote = document.getElementById('aAudio');
+    var dNote = document.getElementById('dAudio');
+    var gNote = document.getElementById('gAudio');
+    var bNote = document.getElementById('bAudio');
+    var ENote = document.getElementById('EAudio');
+    $('#e').mousedown(function () {
+        eNote.currentTime = 0;
+        eNote.play();
+    });
+    $('#a').mousedown(function () {
+        aNote.currentTime = 0;
+        aNote.play();
+    });
+    $('#d').mousedown(function () {
+        dNote.currentTime = 0;
+        dNote.play();
+    });
+    $('#g').mousedown(function () {
+        gNote.currentTime = 0;
+        gNote.play();
+		
+    $('#b').mousedown(function () {
+        bNote.currentTime = 0;
+        bNote.play();
+    });
+    $('#E').mousedown(function () {
+        ENote.currentTime = 0;
+        ENote.play();
+    });
+	});
+	};
+$(document).ready(main);
